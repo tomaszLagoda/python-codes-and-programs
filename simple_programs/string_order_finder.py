50e372e95665d5d3b54f58c578da9eaffba7e867
Tomasz Lagoda <tomek@x-studio.pl>
2024-09-04 16:54:29 +0200

    Program update

--- simple_programs/string_order_finder.py
+++ simple_programs/string_order_finder.py
@@ -26,6 +26,9 @@ expected_results = [
     'jnq'
 ]
 
+""" 
+    This function is is using additional letter indexing
+"""
 def streakFinder(input_string : str) -> str:
     alph_order = "abcdefghijklmnopqrstuvwxyz"
     result = ""
@@ -85,12 +88,66 @@ def streakFinder(input_string : str) -> str:
 
     return result
    
-   
+""" 
+    This function is is using in build python string properties where strings can be directly compared
+    This approach save one while loop
+"""
+def streakFinderV2(input_string : str) -> str:
+    alph_order = "abcdefghijklmnopqrstuvwxyz"
+    result = ""
+    streak = ""
+    letter_to_compare = None
+
+    while True:
+        current_letter = input_string[:1]
+
+        # Check if result is empty - if it is first iteration
+        if not len(result):
+            # Take first letter and store it as a streak
+            streak = current_letter
+           
+            # Store current result
+            result = streak
+           
+            # Store letter for comparison
+            letter_to_compare = current_letter
+        else:  
+            # Streak is not empty, grab first letter and check if that letter is next in the alphabet
+            if (
+               ( current_letter >= letter_to_compare) or
+               ( current_letter == letter_to_compare )
+            ):
+                streak += current_letter
+               
+                # Store letter for comparison
+                letter_to_compare = current_letter
+               
+                # Check if result should be updated
+                if len(streak) > len(result):
+                    result = streak
+
+            # End of a matching streak
+            else:
+                letter_to_compare = current_letter
+               
+                # Start a new streak
+                streak = current_letter
+
+        # Remove first letter from string
+        input_string = input_string[1:]
+
+        if not len(input_string):
+            break
+
+    return result
+
 index = 0  
 for input_string in tests:
-    result = streakFinder(input_string)
+    # result = streakFinder(input_string)
+    result = streakFinderV2(input_string)
     if result == expected_results[index]:
         print("For input " + input_string + " longest streak was " + result)
     else:
         print("Incorrect result for " + input_string + " Received: " + result + " Expected to get: " + expected_results[index])
     index += 1
+    print('')
